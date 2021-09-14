321ccc9c4108b099ba937b1325cc0b8d98719434
lucassenazuza <lucassenazuza@gmail.com>
2021-09-14 03:27:26 -0300

    primeiro commit

--- src/main.js
+++ src/main.js
@@ -6,6 +6,6 @@ import axios from 'axios'
 import Maska from 'maska'
 
 //axios.defaults.baseURL = 'http://127.0.0.1:5000';
-axios.defaults.baseURL = 'http://localhost:5000'
+axios.defaults.baseURL = 'https://flaskcadastraweb.herokuapp.com'
 
 createApp(App).use(store).use(Maska).use(router).mount('#app')
